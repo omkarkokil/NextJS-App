b980f6135c04c9877d8685257d6ae05018e93697
omkar <roleplay56p@gmail.com>
2023-06-29 09:28:05 +0530

    full commit1

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server"
 
 export const GET = async (request, { params }) => {
     const { id } = params
+    console.log(id);
     try {
         await connect()
 
